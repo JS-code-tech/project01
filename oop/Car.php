45b9cbb3a4c71cc6d566ec0a5b9c4919a12c27dd
LoneJL <javasj527@gmailcom>
2023-03-18 22:50:35 +0500

    2 - uzgarishlar router

---
+++ oop/Car.php
@@ -0,0 +1,16 @@
+<?php
+class Car{
+    public $name = "Mirshod";
+
+    public function getName()
+    {
+    return $this->name;
+    }
+
+    $yangi = new Car();
+
+    echo $yangi->getName();
+}
+
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/o2', function () {
-    return "HI";
+// Route::get('/o2', function () {
+//     return 
+
+// });
+
+
+Route::get('/oop', function () {
+   include '../oop';
+    
 });
